ee15522f8af1eac6c2e10264ebe8852fca821a56
ddiskandar <dd@smkplusalfarhan.sch.id>
2021-04-30 11:33:57 +0700

    dirty message

--- resources/views/livewire/student/kelayakan-pip.blade.php
+++ resources/views/livewire/student/kelayakan-pip.blade.php
@@ -12,14 +12,14 @@
         <!-- Pilihan Kelas -->
         <div class="col-span-6">
             <x-jet-label for="desc_keluarga" :value="__('Deskripsi Keluarga')" />
-            <x-textarea wire:model="desc_keluarga" id="desc_keluarga" rows="5" class="block w-full mt-1" maxlength=512></x-textarea>
+            <x-textarea wire:model.defer="desc_keluarga" id="desc_keluarga" rows="5" class="block w-full mt-1" maxlength=512></x-textarea>
             <x-jet-input-error for="desc_keluarga" class="mt-2" />
         </div>
 
         <!-- Pilihan Kelas -->
         <div class="col-span-6 sm:col-span-4">
             <x-jet-label for="pip_id" :value="__('Pilihan Kelas')" />
-            <x-select wire:model="pip_id" id="pip_id" name="pip_id" autocomplete="pip_id" class="block w-full px-3 mt-1" required>
+            <x-select wire:model.defer="pip_id" id="pip_id" name="pip_id" autocomplete="pip_id" class="block w-full px-3 mt-1" required>
                 @foreach($pips as $pip)
                 <option value="{{ $pip->id }}">{{ $pip->name }}</option>
                 @endforeach
@@ -33,6 +33,10 @@
         <x-jet-action-message class="mr-3" on="saved">
             {{ __('Berhasil disimpan.') }}
         </x-jet-action-message>
+
+        <x-dirty-message class="mr-3" 
+            target="desc_keluarga, pip_id" 
+        />
 
         <x-jet-button>
             {{ __('Simpan') }}
